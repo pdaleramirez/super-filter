e52184ca71f988c351f2898a6cc7ae3a77ca9e49
preciousdaleramirez <dalefirstpage@gmail.com>
2023-12-30 20:13:12 +0800

    query params for custom saerch

--- src/services/SearchTypes.php
+++ src/services/SearchTypes.php
@@ -742,6 +742,8 @@ class SearchTypes extends Component
             foreach ($fields as $handle => $value) {
                 $fieldType = SuperFilter::$app->searchTypes->getSearchFieldObjectById($handle, true);
 
+                $fieldType->getQueryParams($query, $value);
+
                 $searchParams = $fieldType->getSearchParams($value);
 
                 if ($searchParams) {
